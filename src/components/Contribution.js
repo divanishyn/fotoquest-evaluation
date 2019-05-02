1cc19119bd7193c112817c05b7da8ab473a72052
Dima Ivanishyn <divanishyn@lohika.com>
2019-05-02 22:15:40 +0300

    Add eslint, prettier, lint-staged, husky, pretty-quick. Fix warnings.

--- src/components/Contribution.js
+++ src/components/Contribution.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-class Contribution extends React.Component {
-    render() {
-        const {id, photos} = this.props.data;
-        console.log(this.props.data);
-        return (
-            <div>
-                <h2>Contribution {id}</h2>
-                {photos.map((photo, index) => (
-                    <img key={index} width="150" src={photo.url} alt="" />
-                ) )}
-            </div>
-        )
-    }
+class Contribution extends React.PureComponent {
+  render() {
+    const { data } = this.props;
+    const { id, photos } = data;
+    return (
+      <div>
+        <h2>Contribution {id}</h2>
+        {photos.map(photo => (
+          <img key={photo.url} width="150" src={photo.url} alt="" />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Contribution;
--- src/components/Contributions.js
+++ src/components/Contributions.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Contribution from './Contribution';
 
-class Contributions extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Contributions</h1>
-                {this.props.data.map((record) => (
-                    <Contribution key={record.id} data={record} />
-                ) )}
-            </div>
-        )
-    }
+class Contributions extends React.PureComponent {
+  render() {
+    const { data } = this.props;
+    return (
+      <div>
+        <h1>Contributions</h1>
+        {data.map(record => (
+          <Contribution key={record.id} data={record} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Contributions;
