75bdbc345e131f5846fdb25d867d3c800785cc4a
Dima Ivanishyn <divanishyn@lohika.com>
2019-05-05 20:55:38 +0300

    Update list, details pages

--- src/components/ContributionDetail.js
+++ src/components/ContributionDetail.js
@@ -1,31 +1,203 @@
 import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+
+import Button from '@material-ui/core/Button';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import Paper from '@material-ui/core/Paper';
+
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+  paper: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    marginTop: theme.spacing.unit * 3
+  },
+  photos: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+    marginTop: theme.spacing.unit * 3
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    transform: 'translateZ(0)'
+  },
+  title: {
+    color: theme.palette.primary.light
+  },
+
+  group: {
+    margin: `${theme.spacing.unit}px 0`
+  },
+  form: {
+    marginTop: theme.spacing.unit * 10
+  },
+  textField: {
+    width: 500
+  }
+});
 
 class ContributionDetail extends React.PureComponent {
+  state = {
+    evaluation: 0,
+    comment: ''
+  };
+
+  handleStatusChange = event => {
+    this.setState({ evaluation: event.target.value });
+  };
+
+  handleCommentChange = event => {
+    this.setState({ comment: event.target.value });
+  };
+
+  handleSubmit = () => {
+    const { evaluation, comment } = this.state;
+    const { history } = this.props;
+    fetch('https://demo0929535.mockable.io/evaluate', {
+      method: 'POST',
+      body: JSON.stringify({ evaluation, comment })
+    })
+      .then(res => res.json())
+      .then(() => {
+        history.push('/');
+      });
+  };
+
   render() {
-    const { data, match } = this.props;
+    const { classes, data, match } = this.props;
     const record = data.find(item => {
       return item.id === match.params.id;
     });
     const date = new Date(record.timestamp).toLocaleString();
     return (
       <div>
-        <h1>ContributionDetail {record.id}</h1>
-        <p>
-          <strong>Land cover:</strong> {record.landobservations[0].landcover}
-        </p>
-        <p>
-          <strong>Land use:</strong> {record.landobservations[0].landuse}
-        </p>
-        <p>
-          <strong>Date:</strong> {date}
-        </p>
-        <h2>Photos</h2>
-        {record.photos.map(photo => (
-          <img key={photo.url} width="150" src={photo.url} alt="" />
-        ))}
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Button component={Link} to="/">
+              <SvgIcon
+                className={classes.icon}
+                style={{ color: 'white', fontSize: 30 }}
+              >
+                <path d="M21 11H6.83l3.58-3.59L9 6l-6 6 6 6 1.41-1.41L6.83 13H21z" />
+              </SvgIcon>
+            </Button>
+            <Typography variant="h5" color="inherit">
+              Contribution {record.id}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Paper className={classes.paper}>
+          <Typography variant="h6" color="inherit">
+            Contribution details
+          </Typography>
+          <Typography component="p">
+            <strong>Land cover:</strong> {record.landobservations[0].landcover}
+          </Typography>
+          <Typography component="p">
+            <strong>Land use:</strong> {record.landobservations[0].landuse}
+          </Typography>
+          <Typography component="p">
+            <strong>Date:</strong> {date}
+          </Typography>
+
+          <div className={classes.photos}>
+            <GridList className={classes.gridList} cols={2.5}>
+              {record.photos.map(tile => (
+                <GridListTile key={tile.url}>
+                  <img src={tile.url} alt={tile.title} />
+                  <GridListTileBar
+                    title={tile.direction.toUpperCase()}
+                    classes={{
+                      root: classes.titleBar,
+                      title: classes.title
+                    }}
+                  />
+                </GridListTile>
+              ))}
+            </GridList>
+          </div>
+
+          <div className={classes.form}>
+            <Typography variant="h6" color="inherit">
+              Evaluate the quality of a contribution
+            </Typography>
+            <FormControl component="fieldset" className={classes.formControl}>
+              <RadioGroup
+                aria-label="Evaluation"
+                name="evaluation"
+                className={classes.group}
+                onChange={this.handleStatusChange}
+              >
+                <FormControlLabel
+                  value="1"
+                  control={<Radio />}
+                  label="Approved"
+                />
+                <FormControlLabel
+                  value="2"
+                  control={<Radio />}
+                  label="Rejected because of photos"
+                />
+                <FormControlLabel
+                  value="3"
+                  control={<Radio />}
+                  label="Rejected because of classifications"
+                />
+                <FormControlLabel
+                  value="4"
+                  control={<Radio />}
+                  label="Rejected because of location"
+                />
+                <FormControlLabel
+                  value="9"
+                  control={<Radio />}
+                  label="Rejected"
+                />
+              </RadioGroup>
+            </FormControl>
+            <FormControl component="fieldset" className={classes.formControl}>
+              <TextField
+                id="comment"
+                label="Comment"
+                multiline
+                rowsMax="6"
+                onChange={this.handleCommentChange}
+                className={classes.textField}
+                margin="normal"
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                onClick={this.handleSubmit}
+              >
+                Submit
+              </Button>
+            </FormControl>
+          </div>
+        </Paper>
       </div>
     );
   }
 }
 
-export default ContributionDetail;
+export default withStyles(styles)(withRouter(ContributionDetail));
--- src/components/Contributions.js
+++ src/components/Contributions.js
@@ -1,18 +1,150 @@
 import React from 'react';
-import ContributionListItem from './ContributionListItem';
+import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import Paper from '@material-ui/core/Paper';
+
+import Button from '@material-ui/core/Button';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import EnhancedTableHead from './EnhancedTableHead';
+
+const styles = theme => ({
+  paper: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    marginTop: theme.spacing.unit * 3
+  },
+  table: {
+    minWidth: 700
+  }
+});
+
+function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+function getSorting(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => desc(a, b, orderBy)
+    : (a, b) => -desc(a, b, orderBy);
+}
+
+const rows = [
+  { id: 'id', numeric: true, label: 'ID' },
+  { id: 'timestamp', numeric: false, label: 'Date' },
+  {
+    id: 'location.lon',
+    numeric: false,
+    label: 'Coordinates'
+  },
+  {
+    id: 'landobservations[0].landcover',
+    numeric: false,
+    label: 'Landcover'
+  },
+  {
+    id: 'landuse',
+    numeric: false,
+    label: 'Landuse'
+  }
+];
 
 class Contributions extends React.PureComponent {
+  state = {
+    order: 'asc',
+    orderBy: 'id'
+  };
+
+  handleRequestSort = (event, property) => {
+    const orderByNew = property;
+    const { orderBy, order } = this.state;
+    let orderNew = 'desc';
+
+    if (orderBy === property && order === 'desc') {
+      orderNew = 'asc';
+    }
+    this.setState({ order: orderNew, orderBy: orderByNew });
+  };
+
   render() {
-    const { data } = this.props;
+    const { classes, data } = this.props;
+    const { order, orderBy } = this.state;
+
     return (
       <div>
-        <h1>Contributions</h1>
-        {data.map(record => (
-          <ContributionListItem key={record.id} data={record} />
-        ))}
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography variant="h5" color="inherit">
+              Contributions to evaluate
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Paper className={classes.paper}>
+          <Table className={classes.table}>
+            <EnhancedTableHead
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={this.handleRequestSort}
+              rowCount={data.length}
+              rows={rows}
+            />
+            <TableBody>
+              {stableSort(data, getSorting(order, orderBy)).map(record => (
+                <TableRow hover tabIndex={-1} key={record.id}>
+                  <TableCell component="th" scope="row">
+                    {record.id}
+                  </TableCell>
+                  <TableCell>
+                    {new Date(record.timestamp).toLocaleString()}
+                  </TableCell>
+                  <TableCell>
+                    {record.location.lat} {record.location.lon}
+                  </TableCell>
+                  <TableCell>{record.landobservations[0].landcover}</TableCell>
+                  <TableCell>{record.landobservations[0].landuse}</TableCell>
+                  <TableCell>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      className={classes.button}
+                      component={Link}
+                      to={`/contribution/${record.id}`}
+                    >
+                      Evaluate
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
       </div>
     );
   }
 }
 
-export default Contributions;
+export default withStyles(styles)(Contributions);
